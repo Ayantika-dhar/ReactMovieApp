ce937722934eafc8e2bdb13b61f03fb140d6c882
Ayantika <adhar9267@gmail.com>
2024-07-27 17:18:51 +0530

    Updates

---
+++ precache-manifest.5a14a58a6d4b4f267b78955cb3becf6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2503a547d59280cd2125e1110d65bcf3",
+    "url": "/ReactMovieApp/index.html"
+  },
+  {
+    "revision": "7d2725facfe2690124ff",
+    "url": "/ReactMovieApp/static/css/main.7d85a346.chunk.css"
+  },
+  {
+    "revision": "c78d7642a334b0ee93be",
+    "url": "/ReactMovieApp/static/js/2.7f695367.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/ReactMovieApp/static/js/2.7f695367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d2725facfe2690124ff",
+    "url": "/ReactMovieApp/static/js/main.bf93d93f.chunk.js"
+  },
+  {
+    "revision": "ddcf23ed6c46b8bb0526",
+    "url": "/ReactMovieApp/static/js/runtime-main.491f9c7c.js"
+  },
+  {
+    "revision": "b98b9440cdb944acae3b80ee58cdbfe7",
+    "url": "/ReactMovieApp/static/media/search.b98b9440.svg"
+  }
+]);
